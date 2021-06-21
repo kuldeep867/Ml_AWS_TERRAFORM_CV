4f6bc45f8e0796fc3b16f03daab49fa4009c74c5
kuldeep867 <68908688+kuldeep867@users.noreply.github.com>
2021-06-21 19:40:50 +0530

    Create Face Recognition.py

---
+++ Face Recognition.py
@@ -0,0 +1,189 @@
+#here we collect the data
+import cv2
+import numpy as np
+
+# Load HAAR face classifier
+face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# Load functions
+def face_extractor(img):
+    # Function detects faces and returns the cropped face
+    # If no face detected, it returns the input image
+    
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray, 1.3, 5)
+    
+    if faces is ():
+        return None
+    
+    # Crop all faces found
+    for (x,y,w,h) in faces:
+        cropped_face = img[y:y+h, x:x+w]
+
+    return cropped_face
+
+# Initialize Webcam
+cap = cv2.VideoCapture(0)
+count = 0
+
+# Collect 100 samples of your face from webcam input
+while True:
+
+    ret, frame = cap.read()
+    if face_extractor(frame) is not None:
+        count += 1
+        face = cv2.resize(face_extractor(frame), (200, 200))
+        face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+
+        # Save file in specified directory with unique name
+        file_name_path = './kuldeep/user/' + str(count) + '.jpg'
+        cv2.imwrite(file_name_path, face)
+
+        # Put count on images and display live count
+        cv2.putText(face, str(count), (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
+        cv2.imshow('Face Cropper', face)
+        
+    else:
+        print("Face found")
+        pass
+
+    if cv2.waitKey(1) == 13 or count == 100: #13 is the Enter Key
+        break
+        
+cap.release()
+cv2.destroyAllWindows()      
+print("Collecting Samples Complete")
+
+
+
+
+import cv2
+import numpy as np
+from os import listdir
+from os.path import isfile, join
+
+# Get the training data we previously made
+data_path = './kuldeep/user/'
+onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path, f))]
+
+# Create arrays for training data and labels
+Training_Data, Labels = [], []
+
+# Open training images in our datapath
+# Create a numpy array for training data
+for i, files in enumerate(onlyfiles):
+    image_path = data_path + onlyfiles[i]
+    images = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    Training_Data.append(np.asarray(images, dtype=np.uint8))
+    Labels.append(i)
+
+# Create a numpy array for both training data and labels
+Labels = np.asarray(Labels, dtype=np.int32)
+
+# Initialize facial recognizer
+# model = cv2.face.createLBPHFaceRecognizer()
+# NOTE: For OpenCV 3.0 use cv2.face.createLBPHFaceRecognizer()
+# pip install opencv-contrib-python
+# model = cv2.createLBPHFaceRecognizer()
+
+kuldeep_model  = cv2.face_LBPHFaceRecognizer.create()
+# Let's train our model 
+kuldeep_model.train(np.asarray(Training_Data), np.asarray(Labels))
+print("Model trained sucessefully")
+
+
+
+#Here we are going to test our model
+
+import cv2
+import numpy as np
+import os
+
+face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+#we can set parameters here and after that we can call the function and it will send a whatpsapp to given number.
+
+import pywhatkit as py
+from datetime import datetime
+from keyboard import press
+
+class Sendwhatsappmsg:
+    def _init_(self):
+        sendwhatmsg()
+
+def sendwhatmsg(pnum,msg):
+    now = datetime.now()
+    hr = int(now.strftime("%H"))
+    min = int(now.strftime("%M")) + 1
+    py.sendwhatmsg(pnum,msg,hr,min)
+    press('enter')
+    return("Msg will be send within 1 min . . . ")
+
+num="+918107600541"
+msg="hii this is test"
+
+def face_detector(img, size=0.5):
+    
+    # Convert image to grayscale
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray, 1.3, 5)
+    if faces is ():
+        return img, []
+    
+    
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,255),2)
+        roi = img[y:y+h, x:x+w]
+        roi = cv2.resize(roi, (200, 200))
+    return img, roi
+
+
+# Open Webcam
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    image, face = face_detector(frame)
+    
+    try:
+        face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+
+        # Pass face to prediction model
+        # "results" comprises of a tuple containing the label and the confidence value
+        results = kuldeep_model.predict(face)
+        # harry_model.predict(face)
+        
+        if results[1] < 500:
+            confidence = int( 100 * (1 - (results[1])/400) )
+            display_string = str(confidence) + '% Confident it is User'
+            
+        cv2.putText(image, display_string, (100, 120), cv2.FONT_HERSHEY_COMPLEX, 1, (255,120,150), 2)
+        
+        if confidence > 90:
+            cv2.putText(image, "Hey Kuldeep how are you ", (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
+            cv2.imshow('Face Recognition', image )
+            #here first os launch by the aws cli commnand
+            os.system("aws ec2 run-instances --image-id ami-0ad704c126371a549  --instance-type t2.micro  --count 1  --key-name aws_key")
+            #here we are creating 5gib ebs volume 
+            os.system("aws ec2 create-volume --availability-zone ap-south-1a --size 5")
+            #here we are going to launch one more os using terraform code which we have in folder ./kuldeep/user/terraform.
+            os.system("terraform -chdir=./kuldeep/user/terraform  apply -auto-approve ")
+            #here we are sending a message to friend
+            sendwhatmsg(num,msg)
+            break    
+         
+        else:
+            cv2.putText(image, "I dont know, who r u", (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,255), 2)
+            cv2.imshow('Face Recognition', image )
+
+    except:
+        cv2.putText(image, "No Face Found", (220, 120) , cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,255), 2)
+        cv2.putText(image, "looking for face", (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,255), 2)
+        cv2.imshow('Face Recognition', image )
+        pass
+        
+    if cv2.waitKey(10) == 13: #13 is the Enter Key
+        break
+        
+cap.release()
+cv2.destroyAllWindows() 
